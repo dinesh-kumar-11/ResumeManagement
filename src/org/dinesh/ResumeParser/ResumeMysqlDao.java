8ebe1d9f9e5beb18021e2d950ba0231070881077
Dinesh Kumar <dinesh@dineshkumar.(none)>
2013-06-16 11:53:14 +0530

    MYSQL DB Connection

--- src/org/dinesh/ResumeParser/ResumeMysqlDao.java
+++ src/org/dinesh/ResumeParser/ResumeMysqlDao.java
@@ -1,16 +1,21 @@
 package org.dinesh.ResumeParser;
 
 import java.sql.DatabaseMetaData;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ResumeMysqlDao {
 
     private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://localhost/resume_management";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/resume_management";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
     private Connection mysqlDbConnection;
@@ -28,68 +33,162 @@ public class ResumeMysqlDao {
 	}
 	return false;
     }
-    
+
+    public static ResumeMysqlDao getResume() {
+	return new ResumeMysqlDao();
+    }
+
     public ResumeMysqlDao() {
 	mysqlDbConnection = getDBConnection();
+	if (mysqlDbConnection == null) {
+
+	}
+    }
+
+    public void closeConnection() {
+	try {
+	    if (mysqlDbConnection != null) {
+		mysqlDbConnection.close();
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    protected void finalize() {
+	try {
+	    if (mysqlDbConnection != null) {
+		mysqlDbConnection.close();
+	    }
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+    public int insertBatchResume(String filename) throws SQLException {
+	PreparedStatement preparedStatementInsert = null;
+	String insertResume = "INSERT INTO  `resume`"
+		+ "(`filename` ,`created` ,`changed`) VALUES" + "(?,?,?)";
+	ResultSet generatedKeys = null;
+	try {
+	    preparedStatementInsert = mysqlDbConnection.prepareStatement(
+		    insertResume, Statement.RETURN_GENERATED_KEYS);
+	    preparedStatementInsert.setString(1, filename);
+	    preparedStatementInsert.setLong(2, getCurrentTimeStamp());
+	    preparedStatementInsert.setLong(3, getCurrentTimeStamp());
+	    preparedStatementInsert.executeUpdate();
+	    generatedKeys = preparedStatementInsert.getGeneratedKeys();
+	    if (generatedKeys.next()) {
+		return (int) generatedKeys.getLong(1);
+	    }
+	} finally {
+	    if (generatedKeys != null)
+		try {
+		    generatedKeys.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	    if (preparedStatementInsert != null)
+		try {
+		    preparedStatementInsert.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	}
+
+	return (Integer) null;
     }
     
+    
+    public int insertResume(String filename) throws SQLException {
+	PreparedStatement preparedStatementInsert = null;
+	String insertResume = "INSERT INTO  `resume`"
+		+ "(`filename` ,`created` ,`changed`) VALUES" + "(?,?,?)";
+	ResultSet generatedKeys = null;
+	try {
+	    preparedStatementInsert = mysqlDbConnection.prepareStatement(
+		    insertResume, Statement.RETURN_GENERATED_KEYS);
+	    preparedStatementInsert.setString(1, filename);
+	    preparedStatementInsert.setLong(2, getCurrentTimeStamp());
+	    preparedStatementInsert.setLong(3, getCurrentTimeStamp());
+	    preparedStatementInsert.executeUpdate();
+	    generatedKeys = preparedStatementInsert.getGeneratedKeys();
+	    if (generatedKeys.next()) {
+		return (int) generatedKeys.getLong(1);
+	    }
+	} finally {
+	    if (generatedKeys != null)
+		try {
+		    generatedKeys.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	    if (preparedStatementInsert != null)
+		try {
+		    preparedStatementInsert.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	}
+
+	return (Integer) null;
+    }
+
+    public int updateResume(String filename, int rid) throws SQLException {
+	PreparedStatement preparedStatementUpdate = null;
+	String insertResume = "UPDATE `resume` SET filename =? , changed=?"
+		+ "WHERE `rid`  = ?";
+	ResultSet generatedKeys = null;
+	try {
+	    preparedStatementUpdate = mysqlDbConnection.prepareStatement(
+		    insertResume, Statement.RETURN_GENERATED_KEYS);
+	    preparedStatementUpdate.setString(1, filename);
+	    preparedStatementUpdate.setLong(2, getCurrentTimeStamp());
+	    preparedStatementUpdate.setInt(3, rid);
+	    preparedStatementUpdate.executeUpdate();
+	    generatedKeys = preparedStatementUpdate.getGeneratedKeys();
+	    if (generatedKeys.next()) {
+		return (int) generatedKeys.getLong(1);
+	    }
+	} finally {
+	    if (generatedKeys != null)
+		try {
+		    generatedKeys.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	    if (preparedStatementUpdate != null)
+		try {
+		    preparedStatementUpdate.close();
+		} catch (SQLException logOrIgnore) {
+		}
+	}
+
+	return (Integer) null;
+    }
+
+    public void startTransaction() throws SQLException {
+	if (mysqlDbConnection != null)
+	    mysqlDbConnection.setAutoCommit(false);
+    }
+
+    public void endTrasaction() throws SQLException {
+	if (mysqlDbConnection != null)
+	    mysqlDbConnection.commit();
+    }
+
+    public void rollback() throws SQLException {
+	if (mysqlDbConnection != null)
+	    mysqlDbConnection.rollback();
+    }
 
     public static void main(String[] argv) throws SQLException {
-
-	Connection dbConnection = null;
-	PreparedStatement preparedStatementInsert = null;
-	PreparedStatement preparedStatementUpdate = null;
-
-	String insertTableSQL = "INSERT INTO DBUSER"
-		+ "(USER_ID, USERNAME, CREATED_BY, CREATED_DATE) VALUES"
-		+ "(?,?,?,?)";
-
-	String updateTableSQL = "UPDATE DBUSER SET USERNAME =? "
-		+ "WHERE USER_ID = ?";
-
+	ResumeMysqlDao resume = null;
 	try {
-	    dbConnection = getDBConnection();
-
-	    dbConnection.setAutoCommit(false);
-
-	    preparedStatementInsert = dbConnection
-		    .prepareStatement(insertTableSQL);
-	    preparedStatementInsert.setInt(1, 999);
-	    preparedStatementInsert.setString(2, "mkyong101");
-	    preparedStatementInsert.setString(3, "system");
-	    preparedStatementInsert.setTimestamp(4, getCurrentTimeStamp());
-	    preparedStatementInsert.executeUpdate();
-
-	    preparedStatementUpdate = dbConnection
-		    .prepareStatement(updateTableSQL);
-	    // preparedStatementUpdate.setString(1,
-	    // "A very very long string caused db error");
-	    preparedStatementUpdate.setString(1, "new string");
-	    preparedStatementUpdate.setInt(2, 999);
-	    preparedStatementUpdate.executeUpdate();
-
-	    dbConnection.commit();
-
-	    System.out.println("Done!");
-
-	} catch (SQLException e) {
-
-	    System.out.println(e.getMessage());
-	    dbConnection.rollback();
+	    resume = ResumeMysqlDao.getResume();
+	    resume.startTransaction();
+	    int lastid = resume.insertResume("test");
+	    resume.endTrasaction();
+	    System.out.println("Done!" + lastid);
 
 	} finally {
-
-	    if (preparedStatementInsert != null) {
-		preparedStatementInsert.close();
-	    }
-
-	    if (preparedStatementUpdate != null) {
-		preparedStatementUpdate.close();
-	    }
-
-	    if (dbConnection != null) {
-		dbConnection.close();
-	    }
+	    resume.closeConnection();
 
 	}
 
@@ -97,7 +196,7 @@ public class ResumeMysqlDao {
 
     private static Connection getDBConnection() {
 
-	Connection dbConnection = null;
+	Connection mysqlDbConnection = null;
 
 	try {
 
@@ -111,9 +210,9 @@ public class ResumeMysqlDao {
 
 	try {
 
-	    dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
-		    DB_PASSWORD);
-	    return dbConnection;
+	    mysqlDbConnection = DriverManager.getConnection(DB_CONNECTION,
+		    DB_USER, DB_PASSWORD);
+	    return mysqlDbConnection;
 
 	} catch (SQLException e) {
 
@@ -121,15 +220,12 @@ public class ResumeMysqlDao {
 
 	}
 
-	return dbConnection;
+	return mysqlDbConnection;
 
     }
 
-    private static java.sql.Timestamp getCurrentTimeStamp() {
-
-	java.util.Date today = new java.util.Date();
-	return new java.sql.Timestamp(today.getTime());
+    private static long getCurrentTimeStamp() {
+return System.currentTimeMillis() / 1000L;
 
     }
-
 }
